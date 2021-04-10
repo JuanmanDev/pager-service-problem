d945e7e1efe3b62af1552e8ea990aa381b5782d4
Juan Manuel Béc <juanmabs22@hotmail.com>
2021-04-10 18:20:44 +0200

    TypeChecking not used in CRUD system

--- src/implementation/DomainLogicServerless/DomainLogicDeleteServiceServerless.ts
+++ src/implementation/DomainLogicServerless/DomainLogicDeleteServiceServerless.ts
@@ -8,6 +8,8 @@ export const DomainLogicDeleteServiceServerless = async function DomainLogicDele
   service: Service,
 ) {
   // Shoild check the service exist before delete? or the Persistante will throw the correct exception?
+  // Update: TypeChecking shoul be before this layer at this time
+  // Example: Use this library to decode de input data: https://gcanti.github.io/io-ts/
 
   await deleteService(service);
   // TODO: catch posible exceptions
